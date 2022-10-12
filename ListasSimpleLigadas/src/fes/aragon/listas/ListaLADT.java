c95d231930213b9e7037e4abaf320265ae80610d
Gaborivas22 <65317629+Gaborivas22@users.noreply.github.com>
2022-10-12 17:33:38 -0500

    Update ListaLADT.java

--- ListasSimpleLigadas/src/fes/aragon/listas/ListaLADT.java
+++ ListasSimpleLigadas/src/fes/aragon/listas/ListaLADT.java
@@ -2,105 +2,98 @@ package fes.aragon.listas;
 
 public class ListaLADT<T> {
 	Nodo<T> head;
-	int tamanio;
-
+	int length;
 	public ListaLADT() {
 	}
-
-
-	public boolean isEmpty() {
-		System.out.println(this.head == null);
-		return this.head == null;
-	}
-
-
-	public int getLength() {
-		System.out.println("El tama\u00f1o de la lista es: " + this.tamanio);
-		return this.tamanio;
-	}
-
-
-	public void addLast(T valor) {
-		if (this.head == null) {
-			head = new Nodo<>(valor);
-		} else {
-			Nodo<T> nuevo = new Nodo(valor);
-			Nodo<T> aux = this.head;
-			while (aux.getNext() != null) {
-				aux = aux.getNext();
-			}
-			aux.setNext(nuevo);
-			tamanio++;
+	public void mostrarVert() {
+		Nodo<T> aux = this.head;
+		while (aux != null) {
+			System.out.print(aux);
+			aux = aux.getNext();
 		}
+		System.out.print("\n");
 	}
-
+	public int getLength() {
+		System.out.println("El tama\u00f1o de nuestra lista es: " + this.length);
+		return this.length;
+	}
 	public void addFirst(T valor) {
 		if (this.head == null) {
 			head = new Nodo<>(valor);
 		} else {
-			Nodo<T> nuevo = new Nodo(valor);
-			nuevo.setNext(head);
-			head = nuevo;
-			tamanio++;
+			Nodo<T> new1 = new Nodo<T>(valor);
+			new1.setNext(head);
+			head = new1;
+			length++;
 		}
 	}
-
+	public void addLast(T valor) {
+		if (this.head == null) {
+			head = new Nodo<>(valor);
+		} else {
+			Nodo<T> new2 = new Nodo<T>(valor);
+			Nodo<T> aux = this.head;
+			while (aux.getNext() != null) {
+				aux = aux.getNext();
+			}
+			aux.setNext(new2);
+			length++;
+		}
+	}
 	public void addAt(T referencia, T valor) {
 		if (this.head == null) {
 			System.out.println("La lista se encuentra vacia");
 			head = new Nodo<>(valor);
 		} else {
-			Nodo<T> nuevo = new Nodo(valor);
-
+			Nodo<T> new2 = new Nodo<T>(valor);
 			Nodo<T> aux = this.head;
-			Nodo<T> aux2 = this.head.getNext();
-
+			Nodo<T> auxi = this.head.getNext();
 			while (aux.getDato() != referencia) {
 				aux = aux.getNext();
-				aux2 = aux2.getNext();
+				auxi = auxi.getNext();
 			}
-			aux.setNext(nuevo);
-			aux.getNext().setNext(aux2);
-			tamanio++;
+			aux.setNext(new2);
+			aux.getNext().setNext(auxi);
+			length++;
 		}
 	}
-
-	public void delete(int posicion) {
-		int posi = 0;
+	public boolean isEmpty() {
+		System.out.println(this.head == null);
+		return this.head == null;
+	}
+	public void delete(int pos1) {
+		int pos = 0;
 		if (head == null) {
 			System.out.println("La lista se encuentra vacia, no es posible borrar un elemento");
 		} else {
-			Nodo<T> aux_anterior = this.head;
+			Nodo<T> auxante = this.head;
 			Nodo<T> aux = this.head.getNext();
-			while (posi != posicion) {
-				posi++;
+			while (pos != pos1) {
+				pos++;
 				aux = aux.getNext();
-				aux_anterior = aux_anterior.getNext();
+				auxante = auxante.getNext();
 			}
-			aux_anterior.setDato(aux.getDato());
-			aux_anterior.setNext(aux.getNext());
-			tamanio--;
+			auxante.setDato(aux.getDato());
+			auxante.setNext(aux.getNext());
+			length--;
 		}
 	}
-
 	public void deleteFirst() {
-		Nodo<T> aux = this.head;
-		aux.setDato(head.getNext().getDato());
-		aux.setNext(head.getNext().getNext());
-		tamanio--;
+		Nodo<T> aux1 = this.head;
+		aux1.setDato(head.getNext().getDato());
+		aux1.setNext(head.getNext().getNext());
+		length--;
 	}
-	//borrar al final
 	public void deleteLast() {
-		Nodo<T> nuevo = this.head.getNext();
-		Nodo<T> aux = this.head;
-		while (nuevo.getNext() != null) {
-			aux = aux.getNext();
-			nuevo = nuevo.getNext();
+		Nodo<T> nuevo1 = this.head.getNext();
+		Nodo<T> aux2 = this.head;
+		while (nuevo1.getNext() != null) {
+			aux2 = aux2.getNext();
+			nuevo1 = nuevo1.getNext();
 		}
-		aux.setNext(null);
-		tamanio--;
+		aux2.setNext(null);
+		length--;
 	}
-
 	public void search(T valor) {
 		Nodo<T> aux = this.head;
 		int indice = 0;
@@ -109,12 +102,11 @@ public class ListaLADT<T> {
 				aux = aux.getNext();
 				indice++;
 			}
-			System.out.println("El dato '" + aux.getDato() + "' se encuentra en la posicion: " + indice);
+			System.out.println("El dato " + aux.getDato() + " se encuentra en la posicion: " + indice);
 		} catch (Exception e) {
 			System.out.println("El dato " + valor + "no ha sido localizado en la lista");
 		}
 	}
-
 	public void actualizar(T a_buscar, T valor) {
 		Nodo<T> aux = this.head;
 		try {
@@ -124,17 +116,6 @@ public class ListaLADT<T> {
 			aux.setDato(valor);
 		} catch (Exception e) {
 			System.out.println(
-					"El dato " + a_buscar + " no ha sido localizado en la lista");
+					"El dato " + a_buscar + " para su actualizacion, no ha sido localizado en la lista");
 		}
 	}
-
-	public void mostrarVert() {
-		Nodo<T> aux = this.head;
-		while (aux != null) {
-			System.out.print(aux);
-			aux = aux.getNext();
-		}
-		System.out.print("\n");
-	}
-
-}
